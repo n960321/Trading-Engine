5a68ed870a208dc86e3fbf22749823917eff19fb
h_Xian <n960321@gmail.com>
2024-05-07 23:40:40 +0800

    add matchorder a million order test

--- test/order_book_test.go
+++ test/order_book_test.go
@@ -13,8 +13,8 @@ import (
 // 加入多筆訂單 確認order book 中的順序是對的
 func Test_AddOrders(t *testing.T) {
 
-	sellOrders := createOrder(RandomInt(10000, 10000000), model.Sell, 1, 100, 10, 200)
-	buyOrders := createOrder(RandomInt(10000, 10000000), model.Buy, 1, 100, 10, 200)
+	sellOrders := createOrder(RandomInt(10000, 10000000), model.OrderSideSell, 1, 100, 10, 200)
+	buyOrders := createOrder(RandomInt(10000, 10000000), model.OrderSideBuy, 1, 100, 10, 200)
 
 	sellResult := make([]*model.Order, len(sellOrders))
 	copy(sellResult, sellOrders)
@@ -52,7 +52,7 @@ func Test_AddOrders(t *testing.T) {
 		{
 			name: "加入多筆掛賣訂單 確認order book 中掛賣的順序是對的",
 			args: args{
-				side:   model.Sell,
+				side:   model.OrderSideSell,
 				orders: sellOrders,
 				result: sellResult,
 			},
@@ -60,7 +60,7 @@ func Test_AddOrders(t *testing.T) {
 		{
 			name: "加入多筆掛買訂單 確認order book 中掛買的順序是對的",
 			args: args{
-				side:   model.Buy,
+				side:   model.OrderSideBuy,
 				orders: buyOrders,
 				result: buyResult,
 			},
@@ -77,9 +77,9 @@ func Test_AddOrders(t *testing.T) {
 			var orderInOrderBook []*model.Order
 
 			switch tt.args.side {
-			case model.Buy:
+			case model.OrderSideBuy:
 				orderInOrderBook = orderBook.GetAllBuyOrders()
-			case model.Sell:
+			case model.OrderSideSell:
 				orderInOrderBook = orderBook.GetAllSellOrders()
 			}
 			t.Logf("Total orders : %d", len(orderInOrderBook))
@@ -95,15 +95,48 @@ func Test_AddOrders(t *testing.T) {
 			}
 		})
 	}
-
 }
+
+
+func Test_MatchOrders(t *testing.T) {
+	orders := createOrder(RandomInt(1000000, 1000000), model.OrderSideUnknow, 1, 100, 1, 100)
+	type args struct {
+		orders []*model.Order
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "加入一百萬筆訂單，嘗試撮合看可以幾秒內完成",
+			args: args{
+				orders: orders,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Logf("orders len: %d",len(tt.args.orders))
+		t.Run(tt.name, func(t *testing.T) {
+			orderBook := model.NewOrderBook()
+			for _, order := range orders {
+				orderBook.Match(order)
+			}
+		})
+	}
+}
+
 
 func createOrder(q int64, side model.OrderSide, priceMin, priceMax, amountMin, amountMax int64) []*model.Order {
 	orders := make([]*model.Order, 0, q)
 	for i := int64(0); i < q; i++ {
+		curSide := model.OrderSideUnknow
+		if side == model.OrderSideUnknow {
+			curSide = model.OrderSide(RandomInt(1, 2))
+		}
 		orders = append(orders, &model.Order{
 			ID:        uint64(i),
-			Side:      side,
+			Side:      curSide,
 			Amount:    decimal.NewFromInt(RandomInt(amountMin, amountMax)),
 			Price:     decimal.NewFromInt(RandomInt(priceMin, priceMax)),
 			CreatedAt: time.Now(),
