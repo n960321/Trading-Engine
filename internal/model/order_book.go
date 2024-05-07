d6bc32c7ffbdaf54280099c0dbcc195843536b56
h_Xian <n960321@gmail.com>
2024-05-07 22:18:08 +0800

    implement the match logic in order book

--- internal/model/order_book.go
+++ internal/model/order_book.go
@@ -2,30 +2,38 @@ package model
 
 import (
 	"sync"
+	"time"
+
+	"github.com/shopspring/decimal"
 )
 
 type OrderBook struct {
+	Mux       sync.Mutex
 	buyQueue  OrderQueueior
 	sellQueue OrderQueueior
-	mu        sync.Mutex
 }
 
 type OrderQueueior interface {
+	// 新增訂單
 	AddOrder(*Order)
+	// 移除訂單
 	RemoveOrder(*Order)
+	// 取得全部訂單
 	GetAllOrders() []*Order
+	// 從最優價格開始取得訂單到給入的price為止，且Total Amount等於或最後一筆訂單加上去會超過 Amount
+	GetOrdersBetweenPirceWithAmount(price, laveAmount decimal.Decimal) []*Order
 }
 
 func NewOrderBook() *OrderBook {
 	return &OrderBook{
+		Mux:       sync.Mutex{},
 		buyQueue:  NewPriceTree(Buy),
 		sellQueue: NewPriceTree(Sell),
 	}
 }
 
+// 將訂單加入訂單簿但不進行搓合
 func (b *OrderBook) AddOrder(order *Order) {
-	b.mu.Lock()
-	defer b.mu.Unlock()
 	switch order.Side {
 	case Buy:
 		b.buyQueue.AddOrder(order)
@@ -34,27 +42,85 @@ func (b *OrderBook) AddOrder(order *Order) {
 	}
 }
 
+// 取得所有掛買單
 func (b *OrderBook) GetAllBuyOrders() []*Order {
 	return b.buyQueue.GetAllOrders()
 }
 
+// 取得所有掛賣單
 func (b *OrderBook) GetAllSellOrders() []*Order {
 	return b.sellQueue.GetAllOrders()
 }
 
-func (b *OrderBook) Match(order *Order) {
+// 將訂單與訂單簿進行撮合
+func (b *OrderBook) Match(order *Order) *MatchResult {
+	b.Mux.Lock()
+	defer b.Mux.Unlock()
+	var makers []*Order
 	switch order.Side {
 	case Buy:
-		// 找賣的本子有沒有
-		// 有
-		// 從最早的時間開始取order來配對
-		// 沒有，加進買的本子等之後搓合
+		makers = b.sellQueue.GetOrdersBetweenPirceWithAmount(order.Price, order.GetLaveAmount())
 	case Sell:
-		// 找買的本子有沒有
-		// 有
-		// 從最早的時間開始取order來配對
-		// 沒有，加進賣的本子等之後搓合
+		makers = b.buyQueue.GetOrdersBetweenPirceWithAmount(order.Price, order.GetLaveAmount())
 	}
-}
 
-// func (b *OrderBook)
+	if len(makers) == 0 {
+		// log something
+		b.AddOrder(order)
+		return &MatchResult{Taker: order}
+	}
+	trades := make([]*Trade, 0)
+	for _, maker := range makers {
+		// 當 taker 剩餘Amount 大於等於 maker的LaveAmount
+		if order.GetLaveAmount().Compare(maker.GetLaveAmount()) >= 0 {
+			trades = append(trades, &Trade{
+				TakerID:   order.ID,
+				MakerID:   maker.ID,
+				TakerSide: order.Side,
+				Amount:    maker.GetLaveAmount(),
+				Price:     maker.Price,
+				CreatedAt: time.Now(),
+			})
+
+			order.MatchAmount.Add(maker.GetLaveAmount())
+			maker.MatchAmount.Add(maker.GetLaveAmount())
+
+		} else {
+			// 當 taker 剩餘Amount 小於 maker的LaveAmount
+			trades = append(trades, &Trade{
+				TakerID:   order.ID,
+				MakerID:   maker.ID,
+				TakerSide: order.Side,
+				Amount:    order.GetLaveAmount(),
+				Price:     maker.Price,
+				CreatedAt: time.Now(),
+			})
+
+			order.MatchAmount.Add(order.GetLaveAmount())
+			maker.MatchAmount.Add(order.GetLaveAmount())
+			// 由於最後一個還有為搓合的數量，所以移除掉
+			makers = makers[:len(makers)-1]
+		}
+	}
+	// taker 剩餘Amount 不是為零 則需要再放進訂單簿中等待搓合
+	if !order.GetLaveAmount().Equal(decimal.Zero) {
+		b.AddOrder(order)
+	}
+	
+	// 從訂單簿中移除已經搓合完成的makers
+	for _, maker := range makers {
+		switch maker.Side {
+		case Buy:
+			b.buyQueue.RemoveOrder(maker)
+		case Sell:
+			b.sellQueue.RemoveOrder(maker)
+		}
+	}
+
+	return &MatchResult{
+		Taker:        order,
+		FinishMakers: makers,
+		Trades:       trades,
+	}
+
+}
---
+++ internal/model/trade.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+type Trade struct {
+	ID        uint64
+	TakerID   uint64
+	MakerID   uint64
+	TakerSide OrderSide
+	Amount    decimal.Decimal
+	Price     decimal.Decimal
+	CreatedAt time.Time
+}
+
+type MatchResult struct {
+	Taker        *Order
+	FinishMakers []*Order
+	Trades       []*Trade
+}
