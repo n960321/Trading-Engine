36e0c3c41612c05ad68856684f872239ce97437a
h_Xian <n960321@gmail.com>
2024-05-12 11:54:50 +0800

    update mysql init

--- deployment/mysql/init/init.sql
+++ deployment/mysql/init/init.sql
@@ -9,12 +9,12 @@ CREATE TABLE `orders` (
   `amount` decimal(10,2) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
   `match_amount` decimal(10,2) DEFAULT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime(3) DEFAULT NULL,
   `updated_at` datetime(3) DEFAULT NULL,
   `deleted_at` datetime(3) DEFAULT NULL,
-  `completed` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `trades`;
@@ -25,5 +25,7 @@ CREATE TABLE `trades` (
   `amount` decimal(10,2) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
   `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
